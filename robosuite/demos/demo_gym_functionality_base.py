6db760d6ea66d5a1da899469d6ab0189c673e906
Gduret <guillaume.duret@ec-lyon.fr>
2022-04-26 10:08:13 +0200

    running code PO

--- robosuite/demos/demo_gym_functionality_base.py
+++ robosuite/demos/demo_gym_functionality_base.py
@@ -34,6 +34,14 @@ demonstrates how this can be easily achieved by using the GymWrapper.
 import robosuite as suite
 from robosuite.wrappers import GymWrapper
 from robosuite.controllers import load_controller_config
+from stable_baselines3.common.env_checker import check_env
+from sb3_contrib import QRDQN, TQC
+from stable_baselines3 import HerReplayBuffer
+
+from stable_baselines3 import A2C
+
+from datetime import datetime
+import os
 
 import gym
 
@@ -50,13 +58,46 @@ if __name__ == "__main__":
             controller_configs=controller_config,
             use_camera_obs=False,  # do not use pixel observations
             has_offscreen_renderer=False,  # not needed since not using pixel obs
-            has_renderer=True,  # make sure we can render to the screen
+            has_renderer=False,  # make sure we can render to the screen
             reward_shaping=True,  # use dense rewards
             control_freq=20,  # control should happen fast enough so that simulation looks smooth
         )
     )
 
     #env = gym.make('FetchReach-v1')
+
+    now = datetime.today()
+    dt_string = now.strftime("%H_%M_%S")
+    print("date and time =", dt_string)	
+
+    filename = 'test_Lift_Panda_TQC' + dt_string
+
+    #TQC issue with her buffer ? obs must be dict ?
+    hyp = {  'policy': 'MultiInputPolicy' , 'buffer_size': 1000000, 'ent_coef': 'auto', 'batch_size': 1024, 'gamma': 0.95, 'learning_starts': 1000, 'learning_rate':1e-3, 'replay_buffer_class': HerReplayBuffer, 'replay_buffer_kwargs': {'online_sampling': True, 'goal_selection_strategy': 'future', 'n_sampled_goal': 4, 'max_episode_length' : 50}, 'policy_kwargs': {'net_arch': [512, 512, 512], 'n_critics': 2}}
+
+    #PPO ?
+    #hyp = { 'policy': 'MultiInputPolicy' , 'ent_coef': 'auto', 'batch_size': 1024, 'gamma': 0.95, 'learning_rate':1e-3, 'policy_kwargs': {'net_arch': [512, 512, 512]}}
+
+    model = TQC(
+        env=env,
+        tensorboard_log="./a2c_cartpole_tensorboard12/",
+        seed=2246026888,
+        verbose=True,
+        **hyp,
+        )
+
+    
+
+    model.learn(total_timesteps=2000000, tb_log_name=filename)
+
+    replay_buffer_path = os.path.join('trained_models3', 'replay_buffer' + filename + '.pkl')
+
+    model.save('trained_models3/' + filename)
+    env.save('trained_models3/vec_normalize_' + filename + '.pkl')     # Save VecNormalize statistics
+    model.save_replay_buffer(replay_buffer_path)
+
+    
+    check_env(env)
 
     for i_episode in range(100):
         observation = env.reset()
--- robosuite/demos/demo_gym_load.py
+++ robosuite/demos/demo_gym_load.py
@@ -65,59 +65,8 @@ def wrap_env(env):
 
 if __name__ == "__main__":
     # load default controller parameters for Operational Space Control (OSC)
-    controller_config = load_controller_config(default_controller="OSC_POSE") #OSC_POSE OSC_POSITION JOINT_POSITION
+    controller_config = load_controller_config(default_controller="OSC_POSITION") #OSC_POSE OSC_POSITION JOINT_POSITION JOINT_VELOCITY JOINT_TORQUE IK_POSE
 
-    # Notice how the environment is wrapped by the wrapper
-    env = GymWrapper(
-        suite.make(
-            "Lift",
-            robots="Panda",  # use Sawyer robot
-            controller_configs=controller_config,
-            use_camera_obs=False,  # do not use pixel observations
-            has_offscreen_renderer=False,  # not needed since not using pixel obs
-            has_renderer=False,  # make sure we can render to the screen
-            reward_shaping=True,  # use dense rewards
-            control_freq=20,  # control should happen fast enough so that simulation looks smooth
-        )
-    )
-    #env = gym.make('FetchPickAndPlace-v1')
-
-    #check_env(env)
-    env = wrap_env(env)
-    #check_env(env)
-
-    now = datetime.today()
-    dt_string = now.strftime("%H_%M_%S")
-    print("date and time =", dt_string)	
-
-    filename = 'test_Lift_Panda_TQC' + dt_string
-
-    #TQC issue with her buffer ? obs must be dict ?
-    hyp = {  'policy': 'MultiInputPolicy' , 'buffer_size': 1000000, 'ent_coef': 'auto', 'batch_size': 1024, 'gamma': 0.95, 'learning_starts': 1000, 'learning_rate':1e-3, 'replay_buffer_class': HerReplayBuffer, 'replay_buffer_kwargs': {'online_sampling': True, 'goal_selection_strategy': 'future', 'n_sampled_goal': 4, 'max_episode_length' : 50}, 'policy_kwargs': {'net_arch': [512, 512, 512], 'n_critics': 2}}
-
-    #PPO ?
-    #hyp = { 'policy': 'MultiInputPolicy' , 'ent_coef': 'auto', 'batch_size': 1024, 'gamma': 0.95, 'learning_rate':1e-3, 'policy_kwargs': {'net_arch': [512, 512, 512]}}
-
-    model = TQC(
-        env=env,
-        tensorboard_log="./a2c_cartpole_tensorboard12/",
-        seed=2246026888,
-        verbose=True,
-        **hyp,
-        )
-
-    
-
-    model.learn(total_timesteps=2000000, tb_log_name=filename)
-
-    replay_buffer_path = os.path.join('trained_models3', 'replay_buffer' + filename + '.pkl')
-
-    model.save('trained_models3/' + filename)
-    env.save('trained_models3/vec_normalize_' + filename + '.pkl')     # Save VecNormalize statistics
-    model.save_replay_buffer(replay_buffer_path)
-
-
-    
     env_robo = GymWrapper(
         suite.make(
             "Lift",
@@ -132,10 +81,21 @@ if __name__ == "__main__":
     )
     
     #env_robo = gym.make('FetchPickAndPlaceDense-v1')
+    filename = "test_Lift_Panda_TQC16_52_15"
+    replay_buffer_path = "trained_models3/replay_buffertest_Lift_Panda_TQC16_52_15"
+
+    #osc_position
+    filename = "test_Lift_Panda_OSCPOSITION09_12_48"
+    replay_buffer_path = "trained_models3/replay_buffertest_Lift_Panda_OSCPOSITION09_12_48"
+
+
+    #osc_pose
+    #filename = "test_Lift_Panda_TQC10_50_44"
+    #replay_buffer_path = "trained_models3/replay_buffertest_Lift_Panda_TQC10_50_44"
 
     # Load model
     model = TQC.load('trained_models3/' + filename, env=env_robo)
-    model.load_replay_buffer(replay_buffer_path, truncate_last_traj=True)
+    #model.load_replay_buffer(replay_buffer_path, truncate_last_traj=True)
         
     # Load the saved statistics
     env = DummyVecEnv([lambda : env_robo])
